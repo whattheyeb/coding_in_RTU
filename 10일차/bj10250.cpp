40a26b3828c79fe92eeeab7c7a188517a130b5ed
whattheyeb <overtokki08@gmail.com>
2025-09-13 13:29:40 +0300

    10250번 피드백 버전 추가

--- 10일차/bj10250.cpp
+++ 10일차/bj10250.cpp
@@ -7,29 +7,29 @@ int main(){
 
     cin >> testcase;
 
-    int floor, room, client;
+    // 문제에서 변수에 대해 제공하는 경우, 문제 표기와 동일하게 둬라
+    int H, W, N;
 
-    int clientFloor;
-    int clientRoom;
+    int N_H;
+    int N_W;
 
     for(int i = 0; i < testcase; i++){
-        cin >> floor >> room >> client;
-        clientFloor = 1;
-        clientRoom = 1;
-        for(int j = 1; j < client; j++){
-            if(clientFloor == floor){
-                clientFloor = 1;
-                clientRoom++;
-            }else{
-                clientFloor++;
-            }
-        }
-        if(clientRoom < 10){
-            cout << clientFloor << "0" << clientRoom << endl;
-        }else{
-            cout << clientFloor << clientRoom << endl;
-        }
+        cin >> H >> W >> N;
         
-        
+        // 손님 숫자마다 루프를 도는 것은 불필요함
+        // for(int j = 1; j < N_; j++){
+        //     if(N_H == H){
+        //         N_H = 1;
+        //         N_W++;
+        //     }else{
+        //         N_H++;
+        //     }
+        // }
+
+        // 피드백 버전
+        N_H = ((N - 1) % H) + 1;
+        N_W = ((N - 1) / H) + 1;
+        // H, W 둘다 99 아래이므로 100을 곱하여 출력하는게 안전함
+        cout << N_H * 100 + N_W << '\n';
     }
 }
